2dd29513fb53631f4b54e2cc7bb6334fce528f21
SupreetRonad <62396592+SupreetRonad@users.noreply.github.com>
2021-05-18 03:21:27 +0530

    item card moved to shared

--- puthon/lib/Screens/User/homeScreen.dart
+++ puthon/lib/Screens/User/homeScreen.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:puthon/Screens/Admin/itemCard.dart';
+import 'package:puthon/Shared/itemCard.dart';
 import 'package:puthon/Screens/User/HomeDrawer.dart';
 import 'package:puthon/Shared/loadingScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
