8e30ac231e4077b222c5f2c871b4898e6b7dfee3
SupreetRonad <62396592+SupreetRonad@users.noreply.github.com>
2021-05-20 02:59:44 +0530

    added dev puthon project

--- puthondev/README.md
+++ puthondev/README.md
@@ -1,6 +1,6 @@
 # puthondev
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
---
+++ puthondev/android/app/src/main/kotlin/com/example/puthondev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.puthondev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
