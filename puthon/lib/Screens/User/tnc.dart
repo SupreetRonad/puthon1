fad96e533b40e1a312e4c3cd1ac7d5535f017486
Supreet Ronad <62396592+SupreetRonad@users.noreply.github.com>
2021-08-04 09:53:41 +0530

    Small changes and refactoring

--- puthon/lib/Screens/User/tnc.dart
+++ puthon/lib/Screens/User/tnc.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:puthon/Shared/successBox.dart';
 import 'package:puthon/shared/showMsg.dart';
 
